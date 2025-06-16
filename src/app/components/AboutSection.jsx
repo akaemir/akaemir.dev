0faec56b31f2cdb3aa537da87671742aa9f0b0c5
Emirhan Çelik <153834863+akaemir@users.noreply.github.com>
2025-06-16 21:32:35 +0300

    Portfolio web site preview

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,98 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>C#</li>
+        <li>.NET</li>
+        <li>Microsoft SQL Server</li>
+        <li>PostgreSQL</li>
+        <li>React</li>
+        <li>JavaScript</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Fullstack Developer Bootcamp, Techcareer</li>
+        <li>Sakarya University, Internet and Network Technologies</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Techcareer - Fullstack Developer Bootcamp</li>
+        <li>Techcareer - HTML5, CSS3, Bootstrap5, TailwindCSS, Javascript, Docker, SQL, Git and Github</li>
+        <li>BTK Academy - C#, SQL, HTML5, Git and Github</li>
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/images/about-image.png" width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+            Hi there! I'm a passionate Junior Developer specializing in modern
+            web development technologies. My journey in software development
+            centers around C# .NET for backend development and React for
+            creating dynamic, user-friendly frontend experiences.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              {" "}
+              Certifications{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
