6635ff3eccd2b440db2ac8814dbd385e86378df1
Emirhan Çelik <153834863+akaemir@users.noreply.github.com>
2025-06-16 22:07:52 +0300

    base path settings

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const isProd = process.env.NODE_ENV = 'production';
+const nextConfig = {
+  basePath: isProd ? "/akaemir.dev" : '',
+  output: "export",
+  distDir: "dist",
+  images: {
+    unoptimized: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
