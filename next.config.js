7cbca7b448858ccddd0bfebf3d8606e5e6f2d2b1
Emirhan Çelik <153834863+akaemir@users.noreply.github.com>
2025-06-17 13:52:53 +0300

    out opt. disabled

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   basePath: isProd ? "/akaemir.dev" : '',
-  output: "export",
   images: {
     unoptimized: true,
   },
