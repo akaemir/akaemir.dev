6c100aaeba4ea1ff30a8643c4941a70239ad5b77
Emirhan Çelik <153834863+akaemir@users.noreply.github.com>
2025-06-18 22:54:25 +0300

    open-next folder deleted

--- .open-next/cloudflare/next-env.mjs
+++
@@ -1,3 +0,0 @@
-export const production = {"PUBLIC_CF_TURNSTILE_SITE_KEY":"0x4AAAAAABhGzXSZl8yrK90H","TURNSTILE_SECRET_KEY":"0x4AAAAAABhGzaxSQp3f9tPLc-iPA_4IZ84","RESEND_API_KEY":"re_QtnRpCJN_9RM5LJwKqNLJMQbhpcfSFbio","FROM_EMAIL":"emir@akaemir.dev","TO_EMAIL":"emircelik534@gmail.com"};
-export const development = {"PUBLIC_CF_TURNSTILE_SITE_KEY":"0x4AAAAAABhGzXSZl8yrK90H","TURNSTILE_SECRET_KEY":"0x4AAAAAABhGzaxSQp3f9tPLc-iPA_4IZ84","RESEND_API_KEY":"re_QtnRpCJN_9RM5LJwKqNLJMQbhpcfSFbio","FROM_EMAIL":"emir@akaemir.dev","TO_EMAIL":"emircelik534@gmail.com"};
-export const test = {};
